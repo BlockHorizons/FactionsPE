0c67ddd38058ecc242358b09f2b99cd25aa30183
Chris-Prime <ChrisssPrime@gmail.com>
2017-01-01 00:50:19 +0200

    fixed /f map

--- src/factions/utils/Collections.php
+++ src/factions/utils/Collections.php
@@ -20,13 +20,28 @@
 namespace factions\utils;
 
 use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+
+use factions\manager\Members;
+use factions\manager\Plots;
+use factions\entity\Plot;
 
 class Collections {
+	
+	// ASCII Map
+    CONST MAP_WIDTH = 48;
+    CONST MAP_HEIGHT = 8;
+    CONST MAP_HEIGHT_FULL = 17;
+    CONST MAP_KEY_CHARS = "\\/#?ç¬£$%=&^ABCDEFGHJKLMNOPQRSTUVWXYZÄÖÜÆØÅ1234567890abcdeghjmnopqrsuvwxyÿzäöüæøåâêîûô";
+    CONST MAP_KEY_WILDERNESS = TextFormat::GRAY . "-";
+    CONST MAP_KEY_SEPARATOR = TextFormat::AQUA . "+";
+    CONST MAP_KEY_OVERFLOW = TextFormat::WHITE . "-" . TextFormat::WHITE; # ::MAGIC?
+    CONST MAP_OVERFLOW_MESSAGE = self::MAP_KEY_OVERFLOW . ": Too Many Factions (>" . 107 . ") on this Map.";
 
     public static function getMap(Player $observer, int $width, int $height, int $inDegrees) {
-        $centerPs = new Vector3($observer->x >> 4, 0, $observer->z >> 4);
+        $centerPs = new Plot($observer);
         $map = [];
-        $centerFaction = Plots::get()->getFactionAt($observer);
+        $centerFaction = $centerPs->getOwnerFaction();
         $head = TextFormat::GREEN . " (" . $centerPs->getX() . "," . $centerPs->getZ() . ") " . $centerFaction->getName() . " " . TextFormat::WHITE;
         $head = TextFormat::GOLD . str_repeat("_", (($width - strlen($head)) / 2)) . ".[" . $head . TextFormat::GOLD . "]." . str_repeat("_", (($width - strlen($head)) / 2));
         $map[] = $head;
@@ -34,13 +49,12 @@ class Collections {
         $halfHeight = $height / 2;
         $width = $halfWidth * 2 + 1;
         $height = $halfHeight * 2 + 1;
-        $topLeftPs = new Vector3($centerPs->x + -$halfWidth, 0, $centerPs->z + -$halfHeight);
+        $topLeftPs = new Plot($centerPs->x + -$halfWidth, $centerPs->z + -$halfHeight);
         // Get the compass
         $asciiCompass = ASCIICompass::getASCIICompass($inDegrees, TextFormat::RED, TextFormat::GOLD);
         // Make room for the list of names
         $height--;
-        /** @var Faction[] $fList */
-        $fList = array();
+        $fList = [];
         $chrIdx = 0;
         $overflown = false;
         $chars = self::MAP_KEY_CHARS;
@@ -55,7 +69,7 @@ class Collections {
                 }
                 if (!$overflown && $chrIdx >= strlen(self::MAP_KEY_CHARS)) $overflown = true;
                 $herePs = $topLeftPs->add($dx, 0, $dz);
-                $hereFaction = Plots::get()->getFactionAt(Plots::fromHash($herePs->x . ":" . $herePs->z . ":" . $observer->getLevel()->getName()));
+                $hereFaction = Plots::getFactionAt(Plots::fromHash($herePs->x . ":" . $herePs->z . ":" . $observer->getLevel()->getName()));
                 $contains = in_array($hereFaction, $fList, true);
                 if ($hereFaction->isNone()) {
                     $row .= self::MAP_KEY_WILDERNESS;
@@ -64,7 +78,7 @@ class Collections {
                 } else {
                     if (!$contains) $fList[$chars{$chrIdx++}] = $hereFaction;;
                     $fchar = array_search($hereFaction, $fList);
-                    $row .= $hereFaction->getColorTo(FPlayer::get($observer)) . $fchar;
+                    $row .= $hereFaction->getColorTo(Members::get($observer)) . $fchar;
                 }
             }
             $line = $row; // ... ---------------
@@ -76,7 +90,7 @@ class Collections {
         }
         $fRow = "";
         foreach ($fList as $char => $faction) {
-            $fRow .= $faction->getColorTo(FPlayer::get($observer)) . $char . ": " . $faction->getName() . " ";
+            $fRow .= $faction->getColorTo(Members::get($observer)) . $char . ": " . $faction->getName() . " ";
         }
         if ($overflown) $fRow .= self::MAP_OVERFLOW_MESSAGE;
         $fRow = trim($fRow);
