f34098ebcf6c3f00718a2580bebd3f099921d597
Chris-Prime <ChrisssPrime@gmail.com>
2016-12-31 21:56:59 +0200

    added /f map

---
+++ src/factions/utils/ASCIICompass.php
@@ -0,0 +1,89 @@
+<?php
+/*
+ *   FactionsPE: PocketMine-MP Plugin
+ *   Copyright (C) 2016  Chris Prime
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace factions\utils;
+
+class ASCIICompass {
+
+    const N = 'N';
+    const NE = '/';
+    const E = 'E';
+    const SE = '\\';
+    const S = 'S';
+    const SW = '/';
+    const W = 'W';
+    const NW = '\\';
+    
+    /**
+     * @param int $degrees
+     * @param string $colorActive
+     * @param string $colorDefault
+     */
+    public static function getASCIICompass(int $degrees, string $colorActive, string $colorDefault) : array {
+        $ret = [];
+        $point = self::getCompassPointForDirection($degrees);
+        $row = "";
+        $row .= ($point === self::NW ? $colorActive : $colorDefault) . self::NW;
+        $row .= ($point === self::N ? $colorActive : $colorDefault) . self::N;
+        $row .= ($point === self::NE ? $colorActive : $colorDefault) . self::NE;
+        $ret[] = $row;
+        $row = "";
+        $row .= ($point === self::W ? $colorActive : $colorDefault) . self::W;
+        $row .= $colorDefault . "+";
+        $row .= ($point === self::E ? $colorActive : $colorDefault) . self::E;
+        $ret[] = $row;
+        $row = "";
+        $row .= ($point === self::SW ? $colorActive : $colorDefault) . self::SW;
+        $row .= ($point === self::S ? $colorActive : $colorDefault) . self::S;
+        $row .= ($point === self::SE ? $colorActive : $colorDefault) . self::SE;
+        $ret[] = $row;
+        return $ret;
+    }
+
+    /**
+     * @param int $degrees
+     * @return string|null
+     */
+    public static function getCompassPointForDirection(int $degrees) {
+        $degrees = ($degrees - 180) % 360;
+        if ($degrees < 0)
+            $degrees += 360;
+        if (0 <= $degrees && $degrees < 22.5)
+            return self::N;
+        elseif (22.5 <= $degrees && $degrees < 67.5)
+            return self::NE;
+        elseif (67.5 <= $degrees && $degrees < 112.5)
+            return self::E;
+        elseif (112.5 <= $degrees && $degrees < 157.5)
+            return self::SE;
+        elseif (157.5 <= $degrees && $degrees < 202.5)
+            return self::S;
+        elseif (202.5 <= $degrees && $degrees < 247.5)
+            return self::SW;
+        elseif (247.5 <= $degrees && $degrees < 292.5)
+            return self::W;
+        elseif (292.5 <= $degrees && $degrees < 337.5)
+            return self::NW;
+        elseif (337.5 <= $degrees && $degrees < 360.0)
+            return self::N;
+        else
+            return null;
+    }
+
+}
